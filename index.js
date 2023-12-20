291f419012c3fd7f86d37a994f77c34dd6054961
Fausto Zambrano <fausto@faqx.com>
2023-12-20 18:23:11 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const express = require('express');
+const axios = require("axios");
+const app = express();
+const port = 3000;
+app.use(express.json());
+
+
+app.post('/validate-recaptcha-token', async (req, res) => {
+    try {
+        const secret = process.env.RECAPTCHA_SECRET;
+        const response = req.body.token;
+        const uri = 'https://www.google.com/recaptcha/api/siteverify?'+`secret=${secret}&response=${response}`;
+        const result = await axios({
+            method: 'post',
+            url: uri,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        console.log(result.data);
+        res.json(result.data)
+    } catch (error){
+        console.log(error);
+        return null;
+    }
+  });
+  
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
