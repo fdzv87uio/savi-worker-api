af74f2d837f0f817dbc97ef64d4787dc349563d6
Fausto Zambrano <fausto@faqx.com>
2024-01-09 14:32:42 -0500

    new envs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const axios = require("axios");
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 app.options('*', cors());
@@ -36,5 +36,5 @@ app.post('/validate-recaptcha-token', async (req, res) => {
   
 
 app.listen(port, () => {
-  console.log(`Servers running at http://localhost:${port}`);
+  console.log(`Servers running at port:${port}`);
 });
